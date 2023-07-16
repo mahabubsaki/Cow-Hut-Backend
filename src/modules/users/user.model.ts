f2aa19d1d73cabf2b92df29cd966fab0e2656d29
mahabubsaki <md929820@gmail.com>
2023-07-16 21:40:31 +0600

    [DONE] created user model with the help of schema

---
+++ src/modules/users/user.model.ts
@@ -0,0 +1,5 @@
+import { model } from "mongoose";
+import { IUser, IUserStatics } from './user.interface';
+import { UserSchema } from "./user.schema";
+
+export const User = model<IUser, IUserStatics>('User', UserSchema);
