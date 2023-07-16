4616ec379c9765053800f62e22b8c9bf18f38adb
mahabubsaki <md929820@gmail.com>
2023-07-16 21:22:14 +0600

    [DONE] created all routes for users model

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -3,4 +3,8 @@ import express from 'express';
 const userRouter = express.Router();
 
 userRouter.post('/auth/signup',);
+userRouter.get('/',);
+userRouter.get('/:id',);
+userRouter.patch('/:id',);
+userRouter.delete('/:id');
 export default userRouter;
